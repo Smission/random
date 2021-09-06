101bfd8565f42215287d7fff252276086fec45e2
Smission <48957515+Smission@users.noreply.github.com>
2021-09-06 15:07:20 +0200

    Google Analytics

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React , {useState, useEffect} from 'react';
 import { BrowserRouter, Route} from "react-router-dom";
 import LandingPage from './pages/Home/Landing';
+import About from './pages/About/About';
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from "history"
+
+ReactGA.initialize('G-BR2WQG60FC');  
+
+function logPageView() { 
+    ReactGA.set({ page: window.location.pathname + window.location.search });
+    ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 function Routes() {
+    const [currentPage] = useState(window.location.pathname);
+
+    useEffect( () => {
+        logPageView()
+    }, [currentPage]);
+    
     return (
-        <BrowserRouter>
+        <BrowserRouter history={createBrowserHistory()}>
             <Route path="/" exact component={LandingPage}/>
+            <Route path="/try" exact component={About}/>
         </BrowserRouter>
     )
 }
